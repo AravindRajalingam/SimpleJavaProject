535c4b38170f9325956d6015973d972bad6a93e5
AravindRajalingam <146104883+AravindRajalingam@users.noreply.github.com>
2024-11-13 20:39:02 +0530

    Add files via upload

---
+++ SimpleJavaProject/test/MainTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    @Test
+    public void testAdd() {
+        Main main = new Main();
+        assertEquals(5, main.add(2, 3));
+    }
+}
